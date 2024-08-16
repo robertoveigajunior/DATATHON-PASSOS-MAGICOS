000c30712199294ec1e48768cae50e97ce1cd61d
Roberto Luiz Veiga Junior <robertoveigajunior@gmail.com>
2024-08-16 00:23:18 -0300

    fix .py

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ import seaborn as sns
 df = pd.read_csv("PEDE_PASSOS_DATASET_FIAP.csv", sep=";")
 raw_gdp_df = pd.read_csv(df)
 
-MIN_YEAR = 2020
-MAX_YEAR = 2025
+# MIN_YEAR = 2020
+# MAX_YEAR = 2025
 
-gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+# gdp_df = raw_gdp_df.melt(
+#         ['Country Code'],
+#         [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
+#         'Year',
+#         'GDP',
+#     )
 
 # Remover valores ausentes
 df = df.dropna()
@@ -29,32 +29,32 @@ df = df.drop_duplicates()
 st.write("Primeiras linhas do DataFrame:")
 st.write(df.head())
 
-# Add some spacing
-''
-''
+# # Add some spacing
+# ''
+# ''
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+# min_value = gdp_df['Year'].min()
+# max_value = gdp_df['Year'].max()
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
+# from_year, to_year = st.slider(
+#     'Which years are you interested in?',
+#     min_value=min_value,
+#     max_value=max_value,
+#     value=[min_value, max_value])
 
-countries = gdp_df['Country Code'].unique()
+# countries = gdp_df['Country Code'].unique()
 
-if not len(countries):
-    st.warning("Select at least one country")
+# if not len(countries):
+#     st.warning("Select at least one country")
 
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+# selected_countries = st.multiselect(
+#     'Which countries would you like to view?',
+#     countries,
+#     ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
 
-''
-''
-''
+# ''
+# ''
+# ''
 
 
 # Análise Descritiva
