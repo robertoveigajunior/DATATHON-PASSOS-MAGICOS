a4653399cd8bb0577be4fae5e1371588e7167c01
Roberto Luiz Veiga Junior <robertoveigajunior@gmail.com>
2024-08-15 22:22:58 -0300

    fix .py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
